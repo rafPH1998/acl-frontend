189762c2920de5df2f2de496ee0176368aa0a5a7
Rafael Belchior <rafaelb89.rb@gmail.com>
2024-01-07 16:11:57 -0300

    implementing modal to delete permission

--- src/views/users/EditView.vue
+++ src/views/users/EditView.vue
@@ -1,6 +1,14 @@
 <template>
   <MainView>
     <div v-if="!loading" class="bg-white rounded-lg shadow-lg w-full text-gray-700">
+
+      <ModalDelete 
+        :modal="modal" 
+        :type="'permissão'"
+        @closeModal="modal.show = false" 
+        @delete="deletePermission(modal.permissionName)"
+      />
+
       <form method="POST" class="mt-4">
         <div class="grid gap-4 grid-cols-2 px-6">
 
@@ -57,9 +65,11 @@
                   </label>
                 </p>
               </div>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 stroke-red-600">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-            </svg>
+              <button class="cursor-pointer" @click="openModal(groupName)">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 stroke-red-600">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
@@ -99,6 +109,7 @@ import SppinerView from '@/icons/SppinerView.vue'
 import SaveView from '@/icons/SaveView.vue'
 import BackView from '@/icons/BackView.vue'
 import AddView from '@/icons/AddView.vue'
+import ModalDelete from '@/components/ModalDelete.vue'
 import { toast } from 'vue3-toastify';
 
 const props = defineProps({
@@ -116,6 +127,11 @@ const users = reactive({
   email: '',
   password: '',
 })
+
+const modal = ref({
+    show: false,
+    permissionName: null,
+});
 
 let permissions = ref([])
 const permissionsUser = ref([])
@@ -220,6 +236,13 @@ const checkPermissions = (groupName, description) => {
   return permissionsUser.value.some(permission => permission.name === groupName && permission.description === description);
 }
 
+/* MODAL DELETA PERMISSAO */
+const deletePermission = (permissionName) => {
+  alert("DELETAR PERMISSAO: " + permissionName)
+
+  // só fazer a requisição
+}
+
 /* PEGA TODAS AS PERMISSOES DISPONIVEIS NO SISTEMA */
 const getPermissions = async () => {
   try {
@@ -232,5 +255,11 @@ const getPermissions = async () => {
     console.error(error)
   }
 }
+
+const openModal = (permName) => {
+  console.log(modal.value.permissionName)
+  modal.value.permissionName = permName;
+  modal.value.show = true;
+};
 </script>
  
